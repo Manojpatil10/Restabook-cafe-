3e6c780b9c73ae36340b380304f80f29d2fd7680
Manojpatil10 <manojrpatil1000@gmail.com>
2024-12-21 12:50:58 +0530

    commit

---
+++ Javascript/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+  $(".customer-slider").owlCarousel({
+    // items:1,
+    loop:true,
+    autoplay:true,
+    margin:30,
+    responsive:{
+      0:{
+        items:1
+      },
+      800:{
+        items:2
+      },
+      1200:{
+        items:3
+      }
+    }
+  })
+
+  $(".mobile-nav").slideUp();
+  $(".hamburger").click(function(){
+    $(".mobile-nav").slideToggle("slow");
+  })
+});
+
+const cursor = document.querySelector(".cursor");
+document.addEventListener('mousemove', e => {
+  cursor.setAttribute("style", "top: "+ (e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px")
+})
